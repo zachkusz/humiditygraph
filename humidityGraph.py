7536006b98ddad5dc5c23097bc31b878f2612643
zachkusz <zskusz@gmail.com>
2017-01-19 14:38:15 -0600

    updates tkinter use, adds humidity range function

--- humidityGraph.py
+++ humidityGraph.py
@@ -1,14 +1,16 @@
 from sense_hat import SenseHat
-import tkinter as tk
+from tkinter import * #version 8.6 as of 01/17/17
+from tkinter import ttk
 
-sense = SenseHat()
+sense = SenseHat() 
 sense.set_rotation(270) #changes row graphing to column
 go = False
 #readingHistory = [] #can kep detailed record of each reading
 
-###used to print on color-grid
+
+###8x8 led color-grid
 '''rows0-7 are y axis arrays of each graph point along the x axis
-the sub arrays are [red,green,blue]'''
+the sub arrays are each point's color [red,green,blue]'''
 row0 = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
 row1 = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
 row2 = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
@@ -20,9 +22,25 @@ row7 = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
 ###
 
 
+###function to set humidity range
+def setHumRange(max, min):
+    global p0, p1, p2, p3, p4, p5, p6, p7
+    #does math, cuts down to hundreths as a string, converts back into floating number
+    dist = float('%.2f' % ((max - min) / 7))
+    p7 = max
+    p6 = min + 6 * dist
+    p5 = min + 5 * dist
+    p4 = min + 4 * dist
+    p3 = min + 3 * dist
+    p2 = min + 2 * dist
+    p1 = min + dist
+    p0 = min
+###
+setHumRange(30,14)
+
 ###function to graph humidity
 def graphLastReading(h):
-    #must use global keyword so python doesnt make new fucking local variables
+    #must use global keyword so python doesnt make new local variables
     global row0,row1,row2,row3,row4,row5,row6,row7
     #moves previous readings back 1 before overwriting them, creating a visual memory
     row0 = row1
@@ -33,47 +51,47 @@ def graphLastReading(h):
     row5 = row6
     row6 = row7
 
-    #change humidity ranges by changing values compred to h in the if statements
-    if (h > 30): #7 very high
+    #change humidity ranges by changing values compared to h in the if statements
+    if (h > p7): #7 very high
         row7 = [[0,200,0],[20,180,0],[30,170,0],[50,150,0],
                 [100,100,0],[120,80,0],[140,50,0],[190,10,0]]
-        print('humidity above normal ranges: ' + str(h))
-    elif (h > 29): #6
+        print('Higher than max. Reading: ' + str(h))
+    elif (h > p6): #6
         row7 = [[0,200,0],[20,180,0],[30,170,0],[50,150,0],
                 [100,100,0],[120,80,0],[140,50,0],[0,0,0]]
-        print('reading ' + str(h))
-    elif (h > 28): #5
+        print('Reading ' + str(h))
+    elif (h > p5): #5
         row7 = [[0,200,0],[20,180,0],[30,170,0],[50,150,0],
                 [100,100,0],[120,80,0],[0,0,0],[0,0,0]]
-        print('reading ' + str(h))
-    elif (h > 27): #4 middle high
+        print('Reading ' + str(h))
+    elif (h > p4): #4 middle high
         row7 = [[0,200,0],[20,180,0],[30,170,0],[50,150,0],
                 [100,100,0],[0,0,0],[0,0,0],[0,0,0]]
-        print('reading ' + str(h))
-    elif (h > 26): #3 middle low
+        print('Reading ' + str(h))
+    elif (h > p3): #3 middle low
         row7 = [[0,200,0],[20,180,0],[30,170,0],[50,150,0],
                 [0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-        print('reading ' + str(h))
-    elif (h > 16): #2
+        print('Reading ' + str(h))
+    elif (h > p2): #2
         row7 = [[0,200,0],[20,180,0],[30,170,0],[0,0,0],
                 [0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-        print('reading ' + str(h))
-    elif (h > 15): #1
+        print('Reading ' + str(h))
+    elif (h > p1): #1
         row7 = [[0,200,0],[20,180,0],[0,0,0],[0,0,0],
                 [0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-        print('reading ' + str(h))
-    elif (h > 14): #0 very low
+        print('Reading ' + str(h))
+    elif (h > p0): #0 very low
         row7 = [[0,200,0],[0,0,0],[0,0,0],[0,0,0],
                 [0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-        print('reading ' + str(h))
-    elif (h <= 14): #far below norm
+        print('Reading ' + str(h))
+    elif (h <= p0): #far below norm
         row7 = [[0,0,200],[0,0,0],[0,0,0],[0,0,0],
                 [0,0,0],[0,0,0],[0,0,0],[0,0,0]]
-        print('humidity below normal ranges: ' + str(h))
+        print('Below min. Reading: ' + str(h))
     else: #error message
         row7 = [[200,0,0],[0,0,0],[0,0,0],[0,0,0],
                 [0,0,0],[0,0,0],[0,0,0],[200,0,0]]
-        print('error, humidity ' + str(h))
+        print('Error: ' + str(h))
 ###
 
  
@@ -83,7 +101,7 @@ def readHum():
 
     if (go):
         hum = sense.get_humidity()
-        #readingHistory.append(hum) #see lines 21-22
+        #readingHistory.append(hum) #for keeping records
         graphLastReading(hum)
 
         #repaints the graph images each loop
@@ -96,37 +114,36 @@ def readHum():
                  row6[0],row6[1],row6[2],row6[3],row6[4],row6[5],row6[6],row6[7],
                  row7[0],row7[1],row7[2],row7[3],row7[4],row7[5],row7[6],row7[7]]
         sense.set_pixels(image)
-        top.after(3000, readHum)
+        root.after(3000, readHum)
 '''time in mili-seconds betwen each new reading
 the tkinter documentation states calling the function inside itself is the
 best way to achieve a while loop without breaking the gui window'''
 ###
 
 
-### tkinter gui window
-top = tk.Tk() #sets top window
-top.geometry("500x500+0+0") #size and position
-top.title("Humidity Sensor")
-
+###functions for tkinter buttons
+#humButton
 def senseToggle():
     global go
     if (go == True):
         go = False
-        humButton["text"] = "Start"
     elif (go == False):
         go = True
         readHum()
-        humButton["text"] = "Stop"
+###
 
-humButton = tk.Button(top, text = "Start", command = senseToggle, height = 1, width = 2)
-humButton.grid(row = 0)
 
-#display box is breaking/hiding humbutton. and no lobel shown
-displayBox = tk.LabelFrame(top, text = "Display Options", height = 400, width = 200,
-                           padx = 20, pady = 20)
+### tkinter gui window
+root = Tk()
+root.title("Humidity Sensor")
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
-clearButton = tk.Button(top, text = "Clear Display", command = sense.clear, height = 1, width = 8)
-clearButton.grid(row = 0)
+humButton = ttk.Button(mainframe, text = "On/Off", command = senseToggle)
+humButton.grid(column = 0, row = 1, sticky = S)
 
-top.mainloop() #nothing else can run after this call
+clearButton = ttk.Button(mainframe, text = "Clear Display", command = sense.clear)
+clearButton.grid(column = 0, row = 2, sticky = N)
+
+root.mainloop()
 ###
